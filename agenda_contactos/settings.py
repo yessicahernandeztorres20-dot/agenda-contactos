7761cdf647061c002872edeb1c54e95c705ce183
unknown <Yessica Hernandez>
2025-11-27 18:44:45 -0500

    Añadir Look 'n Feel con Bootstrap y archivos estáticos

--- agenda_contactos/settings.py
+++ agenda_contactos/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
